b40d2517c8ba77443dd650cfd0718ef309f3631a
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-06 01:59:01 +0200

    add Task_0

---
+++ Task_0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("ВВедите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int square = number * number;
+Console.Write($"Квадрат числа {number} равен {square}");
