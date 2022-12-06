960aea28e38eb98a59bf82fe5189cc61492ad1bb
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-06 18:29:25 +0200

    add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер дня недели: ");
+
+int number = int.Parse(Console.ReadLine());
+
+string[] day = new string[8];
+day[0] = "Ну, давайте серьезно!";
+day[1] = "Понедельник";
+day[2] = "Вторник";
+day[3] = "Среда";
+day[4] = "Четверг";
+day[5] = "Пятница";
+day[6] = "Суббота";
+day[7] = "Врскресенье";
+Console.Write($"День {number} это- {day[number]}");
