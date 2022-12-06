ead49cf53d00da5392a8079fc413a6c11f2256d4
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-06 12:54:51 +0200

    add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("ВВедите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int square = numberB * numberB;
+//Console.Write($"Квадрат числа {number} равен {square}");
+if (numberA == square)
+{
+Console.Write($"Первое число {numberA} является квадратом второго число {numberB}");
+ }   
+else
+{
+Console.Write($"Первое число {numberA} не является квадратом второго числа {numberB}");
+ }
