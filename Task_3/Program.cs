aeb1d5d0f761ca719ece662616103e9ad060a3cd
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-06 19:33:26 +0200

    add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N.
+*/
+Console.Clear();
+
+Console.Write("Введите число N: ");
+
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+if(number > 0)
+{
+count = number*(-1);
+    while (count <= number)
+    {
+        Console.Write($"{count}, ");
+        count++;
+    }
+}
+else
+{
+    count=number;
+    while (count<= number*(-1))
+    {
+     Console.Write($"{count}, ");
+    count++;
+    }
+}
