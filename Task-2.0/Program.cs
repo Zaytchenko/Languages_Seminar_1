0e8c923e42716a8cd53125de45063c1655f6ab7c
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-09 11:56:20 +0200

    add Task_2.0

---
+++ Task-2.0/Program.cs
@@ -0,0 +1,11 @@
+﻿// выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+Console.Clear();
+int random = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " +random);
+int numberEnd = random %10;
+int numberOne = random /10;
+Console.WriteLine("Первая цифра числа: " +numberOne);
+Console.WriteLine("Последняя цифра числа: " +numberEnd);
+int max = numberOne;
+if (numberEnd > numberOne) max = numberEnd;
+Console.WriteLine("Максимальная цифра числа: " +max);
