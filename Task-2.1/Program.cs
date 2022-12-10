a16981d1fe130e36f9a9e7f5cca6da679682b816
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-10 13:36:17 +0200

    add Task-2.1

---
+++ Task-2.1/Program.cs
@@ -0,0 +1,6 @@
+﻿//  выводит случайное трехзначное число и удаляет вторую цифру этого числа
+Console.Clear();
+int random = new Random().Next(100, 1000);
+Console.WriteLine("Случайное число: " +random);
+int result = (random /100)*10 + random %10;
+Console.WriteLine(random + "->" + result);
