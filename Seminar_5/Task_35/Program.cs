20e7c17bfd4dc33d7d2aa7139a5f4d1a15fcd8cf
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-19 21:16:18 +0200

    Seminar_5

---
+++ Seminar_5/Task_35/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+const int SIZE = 123;
+const int LEFT_RANGE = 0;
+const int RIGHT_RANGE = 500;
+
+int[] array = new int[SIZE];
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); //[-9;10)
+    }
+    return arr;
+}
+
+array = FillArrayWithRandomNumbers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if ((array[i] >= 10) && (array[i] <= 99))
+    {
+        count += 1;
+    }
+}
+System.Console.WriteLine("Кол-вo эл-тов в заданном диапазоне = " + count);
---
+++ Seminar_5/Task_37/Program.cs
@@ -0,0 +1,36 @@
+﻿// произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
+
+const int SIZE = 11;
+const int LEFT_RANGE = 0;
+const int RIGHT_RANGE = 100;
+
+int[] array = new int[SIZE];
+
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); //[-9;10)
+    }
+    return arr;
+}
+
+array = FillArrayWithRandomNumbers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+int[] newArray = new int[SIZE / 2 + SIZE % 2];
+for (int i = 0; i < SIZE / 2; i++)
+{
+    newArray[i] = array[i] * array[array.Length - 1 - i];
+}
+
+if (array.Length % 2 != 0)
+{
+    newArray[newArray.Length - 1] = array[array.Length / 2];
+}
+Console.WriteLine('[' + string.Join(", ", newArray) + ']');
